46eb6cad48fcc473c1d740c7d8770bbdb971ea55
FastRives <57791635+FastRives@users.noreply.github.com>
2021-06-25 01:10:08 +0200

    Delete all

--- Script.js
+++
@@ -1,6 +0,0 @@
-    const containerMenu = document.querySelector('.container-menu');
-    const btnMenu = document.querySelector('.btn-menu');
-
-    btnMenu.addEventListener('click', () => {
-        containerMenu.classList.toggle('active');
-    })  
